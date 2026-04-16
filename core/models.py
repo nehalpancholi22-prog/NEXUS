4491f18919bb6686b0bbf2d1602e846b9fae88c1
root <root@Nehal-Pancholi.localdomain>
2026-04-17 02:23:33 +0530

    initial commit

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    tech_stack = models.CharField(max_length=200)
+    github_link = models.URLField(blank=True, null=True)
+    live_demo = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
